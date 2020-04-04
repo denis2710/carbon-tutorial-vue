dbb482686290397d67d90f6bf136248b100b82b8
Denis <denis@ibm.com>
2020-04-04 11:29:15 -0300

    feat(tutorial): complete step 1

---
+++ src/views/LandingPage/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Wellcome to Carbon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage'
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/views/RepoPage/RepoPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Repo page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RepoPage'
+};
+</script>
+
+<style lang="scss"></style>
